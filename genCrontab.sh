35b8d1b12b16914fdfe72915e1dc2699b3dea42f
hong <hong@xiyanghui>
2016-04-06 16:51:37 +0800

    Update backup file directory

--- genCrontab.sh
+++ genCrontab.sh
@@ -3,8 +3,8 @@
 PROJECT_DIR=$(cd "$(dirname $0)"; pwd)
 source $(cd "$(dirname $0)"; pwd)/vars.sh
 
-OPLOG_CRONTAB="*/15 * * * * $PROJECT_DIR/dumpoplog.sh 2>&1 >> $BACKUP_DIR/oplog.log"
-FULL_CRONTAB="0 6 * * * $PROJECT_DIR/dumpfull.sh 2>&1 >> $BACKUP_DIR/full.log"
+OPLOG_CRONTAB="*/15 * * * * $PROJECT_DIR/dumpoplog.sh 2>&1 >> $WORK_DIR/oplog.log"
+FULL_CRONTAB="0 6 * * * $PROJECT_DIR/dumpfull.sh 2>&1 >> $WORK_DIR/full.log"
 
 echo "Crontab rule for incremental backup: "
 echo "$OPLOG_CRONTAB"
