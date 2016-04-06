435eeb52095233d280a0fdd0ee0cbd8d85b83eed
hong <hong@xiyanghui>
2016-04-06 15:46:24 +0800

    Add gen crotab rull file

---
+++ genCrontab.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PROJECT_DIR=$(cd "$(dirname $0)"; pwd)
+source $(cd "$(dirname $0)"; pwd)/vars.sh
+
+OPLOG_CRONTAB="*/15 * * * * $PROJECT_DIR/dumpoplog.sh 2>&1 >> $BACKUP_DIR/oplog.log"
+FULL_CRONTAB="0 6 * * * $PROJECT_DIR/dumpfull.sh 2>&1 >> $BACKUP_DIR/full.log"
+
+echo "Crontab rule for incremental backup: "
+echo "$OPLOG_CRONTAB"
+echo "Crontab rule for full backup: "
+echo "$FULL_CRONTAB"
